49870ef94348568eff952305296bf32f5f927035
adarsh1525 <126499149+adarsh1525@users.noreply.github.com>
2025-07-30 15:31:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "aws_eip" "eip" {
+  
+}
+
+
+resource "aws_instance" "test" {
+    instance_type = "t2.micro"
+    ami = "ami-054b7fc3c333ac6d2"
+    tags = {
+      Name = var.tagname[count.index]
+    }
+   # count = 3
+    # instance_type = var.
+}
+
+
+# resource "aws_security_group" "test" {
+#   name = "allow"
+#   description = "test"
+#   tags = {
+#     Name = "allow_test"
+#   }
+  
+#   ingress {
+#     from_port = 80
+#     to_port = 80
+#     protocol = "tcp"
+#     cidr_blocks = [
+#         "0.0.0.0/0"
+#     ]
+#   }
+
+#   egress {
+#     protocol = -1
+#     from_port = 0
+#     to_port = 0
+#     cidr_blocks = [
+#         "0.0.0.0/0"
+#     ]
+#   }
+
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "testi" {
+#   cidr_ipv4 = "${var.ip}/0"
+#   ip_protocol = "tcp"
+#   from_port = var.from_port
+#   to_port = var.from_port
+#   security_group_id = aws_security_group.test.id
+# }
+
