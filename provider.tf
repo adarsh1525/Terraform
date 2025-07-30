6ecbe080aa63e8243d4f0ab57b5f1fa30baa60ee
adarsh1525 <126499149+adarsh1525@users.noreply.github.com>
2025-07-30 16:07:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    digitalocean = {
-        source = "digitalocean/digitalocean"
-        version = "= 2.57.0"
-    }
-  }
-}
-
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-
- # required_version = "values(>= 1.0.0, < 2.0.0)"
-}
 provider "aws" {
   region = "us-west-2"
 
